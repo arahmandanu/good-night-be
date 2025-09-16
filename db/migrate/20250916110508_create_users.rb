ea67a9c39b21badb31391a6a5b1967ed2c3c8668
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-09-16 19:45:20 +0700

    optimize user db

--- db/migrate/20250916110508_create_users.rb
+++ db/migrate/20250916110508_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users, id: :uuid  do |t|
-      t.name
+      t.string :name, null: false
       t.timestamps
     end
   end
---
+++ db/migrate/20250916122620_add_index_to_user_name.rb
@@ -0,0 +1,5 @@
+class AddIndexToUserName < ActiveRecord::Migration[7.2]
+  def change
+    add_index :users, :name, unique: true
+  end
+end
