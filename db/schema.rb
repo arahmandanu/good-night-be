44792b4b44fec3f1d8cc2075f5a41ea7a0c76125
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2025-09-17 14:41:20 +0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_16_180848) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_17_065548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "sleeps", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.datetime "start_time", null: false
+    t.datetime "end_time"
+    t.bigint "duration_seconds"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["end_time"], name: "index_sleeps_on_end_time"
+    t.index ["start_time"], name: "index_sleeps_on_start_time"
+    t.index ["user_id", "start_time"], name: "index_sleeps_on_user_id_and_start_time"
+    t.index ["user_id"], name: "index_sleeps_on_user_id"
+    t.index ["user_id"], name: "index_sleeps_on_user_id_open_sleep", unique: true, where: "(end_time IS NULL)"
+  end
 
   create_table "user_follows", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "user_id", null: false
@@ -31,4 +45,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_16_180848) do
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_users_on_name", unique: true
   end
+
+  add_foreign_key "sleeps", "users"
 end
